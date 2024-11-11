7f1a39a4f50e677ebe461d6a0053f46c77ad0e84
Sude Naz Kalkan <sudee2927@gmail.com>
2024-11-11 20:38:03 +0300

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -3,69 +3,73 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skalkan <skalkan@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bpancar <bpancar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 05:46:32 by skalkan           #+#    #+#             */
-/*   Updated: 2024/11/10 18:59:15 by skalkan          ###   ########.fr       */
+/*   Updated: 2024/11/11 20:34:35 by bpancar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_putchar(char s)
+int	ft_putchar(char s)
 {
-    write(1, &s, 1);
-    return (1);
+	write(1, &s, 1);
+	return (1);
 }
 
-int ft_handle_format(char c, va_list arg)
+int	ft_handle_format(char c, va_list arg)
 {
-    int count;
+	int		count;
+	char	*base;
+	char	*base2;
 
-    count = 0;
-    if (c == 'c')
-        count += ft_putchar(va_arg(arg, int));
-    else if (c == 's')
-        count += ft_putstr(va_arg(arg,char *));
-    else if (c == 'p')
-        count += ft_pointer(va_arg(arg, unsigned long));
-    else if (c == 'd' || c == 'i')
-        count += ft_putnbr(va_arg(arg,int));
-    else if (c == 'u')
-        count += ft_unsigned(va_arg(arg,unsigned int));
-    else if (c == '%')
+	base = "0123456789abcdef";
+	base2 = "0123456789ABCDEF";
+	count = 0;
+	if (c == 'c')
+		count += ft_putchar(va_arg(arg, int));
+	else if (c == 's')
+		count += ft_putstr(va_arg(arg, char *));
+	else if (c == 'p')
+		count += ft_pointer(va_arg(arg, unsigned long));
+	else if (c == 'd' || c == 'i')
+		count += ft_putnbr(va_arg(arg, int));
+	else if (c == 'u')
+		count += ft_unsigned(va_arg(arg, unsigned int));
+	else if (c == '%')
 		count += ft_putchar('%');
-    else if (c == 'x')
-        count += ft_recursive_hex(va_arg(arg,unsigned int),"0123456789abcdef");
-    else if (c == 'X')
-        count += ft_recursive_hex(va_arg(arg,unsigned int),"0123456789ABCDEF");
-    return (count);
+	else if (c == 'x')
+		count += ft_recursive_hex(va_arg(arg, unsigned int), base);
+	else if (c == 'X')
+		count += ft_recursive_hex(va_arg(arg, unsigned int), base2);
+	return (count);
 }
 
-int ft_printf(const char *s, ...)
+int	ft_printf(const char *s, ...)
 {
-    va_list arg;
-    int count;
-    int i;
+	va_list	arg;
+	int		count;
+	int		i;
 
-    count = 0;
-    i = 0;
-    va_start(arg,s);
-    while (s[i])
-    {
-        if (s[i] != '%')
-        {
-            ft_putchar(s[i]);
-            count++;
-        }
-        if (s[i] == '%')
-        {
-            i++;
-            while (s[i] == ' ' && s[i])
-                i++;
-            count += ft_handle_format(s[i],arg);
-        }
-        i++;
-    }
-    return (count);
+	count = 0;
+	i = 0;
+	va_start(arg, s);
+	while (s[i])
+	{
+		if (s[i] != '%')
+		{
+			ft_putchar(s[i]);
+			count++;
+		}
+		if (s[i] == '%')
+		{
+			i++;
+			while (s[i] == ' ' && s[i])
+				i++;
+			count += ft_handle_format(s[i], arg);
+		}
+		i++;
+	}
+	return (count);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -3,27 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skalkan <skalkan@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bpancar <bpancar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 16:29:58 by skalkan           #+#    #+#             */
-/*   Updated: 2024/11/10 18:59:38 by skalkan          ###   ########.fr       */
+/*   Updated: 2024/11/11 20:33:24 by bpancar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
 
-int ft_printf(const char *s, ...);
-int ft_putchar(char s);
-int ft_putnbr(int a);
-int ft_putstr(char *s);
-int ft_unsigned(unsigned int a);
-int ft_pointer(unsigned long p);
-int ft_recursive_hex(unsigned int p, char *base);
-int ft_handle_format(char c, va_list arg);
+int	ft_printf(const char *s, ...);
+int	ft_putchar(char s);
+int	ft_putnbr(int a);
+int	ft_putstr(char *s);
+int	ft_unsigned(unsigned int a);
+int	ft_pointer(unsigned long p);
+int	ft_recursive_hex(unsigned int p, char *base);
+int	ft_handle_format(char c, va_list arg);
 
 #endif
